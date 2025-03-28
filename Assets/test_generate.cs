c90bfd93c62ea929eca8d2b7311bd6c4c4ddcad4
Vlad Kiev <1104581+vlad32768@users.noreply.github.com>
2025-03-28 16:09:26 +0300

    Fixed beam lighting and geometry; added mesh generation to beam

--- Assets/test_generate.cs
+++ Assets/test_generate.cs
@@ -8,35 +8,56 @@ public class test_generate : MonoBehaviour
     MeshRenderer mr;
     public Mesh m;
     public balka_solver bs;
-    public float EI = 1000f;
     public float yc = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    List<Vector3> GenerateIBeam()
+    (List<Vector3>, List<Vector3>) GenerateIBeam()
     {
+        List<Vector3> norm = new List<Vector3>();
         List<Vector3> IBeam = new List<Vector3>();
-        IBeam.Add(new Vector3(0, 0, 0));
-        IBeam.Add(new Vector3(0.3f, 0, 0));
-        IBeam.Add(new Vector3(0.3f, 0.1f, 0));
+        IBeam.Add(new Vector3(0, 0, 0)); norm.Add(Vector3.left);
+        IBeam.Add(new Vector3(0, 0, 0)); norm.Add(Vector3.down);
+        IBeam.Add(new Vector3(0.3f, 0, 0)); norm.Add(Vector3.down);
+        IBeam.Add(new Vector3(0.3f, 0, 0)); norm.Add(Vector3.right);
+        IBeam.Add(new Vector3(0.3f, 0.1f, 0)); norm.Add(Vector3.right);
+        IBeam.Add(new Vector3(0.3f, 0.1f, 0)); norm.Add(Vector3.up);
 
-        IBeam.Add(new Vector3(0.2f, 0.1f, 0));
-        IBeam.Add(new Vector3(0.2f, 0.5f, 0));
-        IBeam.Add(new Vector3(0.3f, 0.5f, 0));
+        IBeam.Add(new Vector3(0.2f, 0.1f, 0)); norm.Add(Vector3.up);
+        IBeam.Add(new Vector3(0.2f, 0.1f, 0)); norm.Add(Vector3.right);
+        IBeam.Add(new Vector3(0.2f, 0.5f, 0)); norm.Add(Vector3.right);
+        IBeam.Add(new Vector3(0.2f, 0.5f, 0)); norm.Add(Vector3.down);
+        IBeam.Add(new Vector3(0.3f, 0.5f, 0)); norm.Add(Vector3.down);
+        IBeam.Add(new Vector3(0.3f, 0.5f, 0)); norm.Add(Vector3.right);
 
-        IBeam.Add(new Vector3(0.3f, 0.6f, 0));
-        IBeam.Add(new Vector3(0, 0.6f, 0));
-        IBeam.Add(new Vector3(0, 0.5f, 0));
+        IBeam.Add(new Vector3(0.3f, 0.6f, 0)); norm.Add(Vector3.right);
+        IBeam.Add(new Vector3(0.3f, 0.6f, 0)); norm.Add(Vector3.up);
+        IBeam.Add(new Vector3(0, 0.6f, 0)); norm.Add(Vector3.up);
+        IBeam.Add(new Vector3(0, 0.6f, 0)); norm.Add(Vector3.left);
+        IBeam.Add(new Vector3(0, 0.5f, 0)); norm.Add(Vector3.left);
+        IBeam.Add(new Vector3(0, 0.5f, 0)); norm.Add(Vector3.down);
 
-        IBeam.Add(new Vector3(0.1f, 0.5f, 0));
-        IBeam.Add(new Vector3(0.1f, 0.1f, 0));
-        IBeam.Add(new Vector3(0f, 0.1f, 0));
+        IBeam.Add(new Vector3(0.1f, 0.5f, 0)); norm.Add(Vector3.down);
+        IBeam.Add(new Vector3(0.1f, 0.5f, 0)); norm.Add(Vector3.left);
+        IBeam.Add(new Vector3(0.1f, 0.1f, 0)); norm.Add(Vector3.left);
+        IBeam.Add(new Vector3(0.1f, 0.1f, 0)); norm.Add(Vector3.up);
+        IBeam.Add(new Vector3(0f, 0.1f, 0)); norm.Add(Vector3.up);
+        IBeam.Add(new Vector3(0f, 0.1f, 0)); norm.Add(Vector3.left);
         float sc = 1f / 3f;
-
+        
+        Vector3 xs = Vector3.right * -0.15f;
+        Vector3 ys = Vector3.up * -0.3f;
+        
         for (int i = 0; i < IBeam.Count; ++i)
         {
+            IBeam[i] += (xs + ys);
             IBeam[i] *= sc;
+            
         }
-        return IBeam;
+        return (IBeam,norm);
+        
+
+        
+
     }
     
     
@@ -45,22 +66,26 @@ public class test_generate : MonoBehaviour
     {
       
         List<Vector3> vtx = new List<Vector3>();
+        List<Vector3> norm = new List<Vector3>();
         List<int> tris = new List<int>();
 
 
         float r = 0.2f;
-        int sctn = 10;
+        int sctn = 1000;
 
 
-        var IBeam = GenerateIBeam();
+        var (IBeam,Norm) = GenerateIBeam();
 
         for (int i = 0; i < sctn; i++)
         {
             Vector3 z = Vector3.forward * 2f / sctn * i;
-            Vector3 y = Vector3.up * bs.CalculateDeflection(z.z);
+            Vector3 y = Vector3.up * - bs.CalculateDeflection(z.z);
+            
+
             for (int j = 0; j < IBeam.Count; j++)
             {
                 vtx.Add(IBeam[j] + z + y);
+                norm.Add(Norm[j]);
             }
         }
         
@@ -68,7 +93,7 @@ public class test_generate : MonoBehaviour
         for (int i = 0; i < sctn - 1; i++)
         {
 
-            for (int j = 0; j < n; j++)
+            for (int j = 1; j < n; j+=2)
             {
                 // <cond>?<exp1>:<exp2>
                 int jnext = j == n - 1 ? 0 : j + 1;
@@ -85,6 +110,7 @@ public class test_generate : MonoBehaviour
         m = new Mesh();
         m.vertices = vtx.ToArray();
         m.triangles = tris.ToArray();
+        m.normals = norm.ToArray();
         m.name = "Beam_Mesh";
     }
     void Start()
